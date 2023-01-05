46a2fbc24c8cb2397d4986bf1e9b2364a6a20fd2
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2023-01-05 18:10:37 +0500

    bug fixed

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,2 +1,2 @@
 from apps.models.stores import Store
-from apps.models.base import Product, Stream
+from apps.models.base import Product, Stream,Comment
--- apps/models/base.py
+++ apps/models/base.py
@@ -3,6 +3,8 @@ from django.db.models import Model, DateTimeField, SlugField, ForeignKey, PROTEC
     BooleanField
 from django.db.models import Model, DateTimeField, SlugField, ForeignKey, PROTECT, TextField, CASCADE
 from django.db.models import CharField
+from django.db.models import Model, DateTimeField, SlugField, ForeignKey, PROTECT, TextField, CharField, CASCADE, \
+    IntegerField
 from django_resized import ResizedImageField
 
 from apps.models import Store
@@ -11,7 +13,7 @@ from apps.models import Store
 class Product(Model):
     title = CharField(max_length=255)
     main_picture = ResizedImageField(size=[500, 300], upload_to='%m')
-    price = CharField(max_length=200)
+    price = IntegerField(max_length=200)
     created_at = DateTimeField(auto_now_add=True)
     slug = SlugField(max_length=255, unique=True)
     store = ForeignKey(Store, CASCADE)
@@ -19,6 +21,7 @@ class Product(Model):
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
+
 
 # TODO Javlon Commentni alohida file ga olib ciqib qoying
 class Comment(Model):
@@ -32,12 +35,3 @@ class Comment(Model):
 
     def __str__(self):
         return self.name
-
-
-class Stream(Model):
-    name = CharField(max_length=255)
-    donation = CharField(max_length=20)                     # hayriya uchun mablag`
-    reduce = CharField(max_length=20)                       # narxini kamaytirish uchun mablag`
-    user = ForeignKey('User', CASCADE)                      # oqim yaratgan foydalanuchi
-    product = ForeignKey('apps.base.Product', CASCADE)      # oqim uchun mahsulot
-    is_area = BooleanField(default=False)                   # hududsiz qabul qilish

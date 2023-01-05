9b0c72b2af994e4021ae4fa456c460ef4e53127f
rashideveloper <bubbless7456@gmail.com>
2023-01-05 17:20:18 +0500

    shop model

---
+++ apps/models/shop.py
@@ -0,0 +1,5 @@
+from django.db.models import Model
+
+
+class Shop(Model):
+    image = 
--- apps/views/base.py
+++ apps/views/base.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
 
 
+class MainView(TemplateView):
+    template_name = 'apps/index.html'
+from django.views.generic import TemplateView
+
+
 class MainPageView(TemplateView):
     template_name = 'apps/main_page.html'

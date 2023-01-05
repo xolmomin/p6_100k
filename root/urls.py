554fec0879f6a241cc2793198934095977ac2e35
Ahrorbek <admin@mail.ru>
2023-01-05 16:32:05 +0500

    edit directory

--- root/urls.py
+++ root/urls.py
@@ -1,4 +1,4 @@
-"""p6_100k URL Configuration
+"""root URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/

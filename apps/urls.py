9f535fcc093c72d9bfb69f727239737fb72be0c2
Yuldoshaliyev1 <akmalyuldoshaliev@gmail.com>
2023-01-05 17:11:06 +0500

    change base.py change settings.py

--- apps/urls.py
+++ apps/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+
+from apps.views.base import MainPageView
 
 urlpatterns = [
-    path('', include('apps.urls')),
-    path('admin/', admin.site.urls),
+    path('', MainPageView.as_view(), name='main_page'),
 ]

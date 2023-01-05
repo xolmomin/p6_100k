3613a6201c887d42f57a8eb47c1d4d013d161032
rashideveloper <bubbless7456@gmail.com>
2023-01-05 17:31:12 +0500

    store admin

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from apps.models.base import Store, Product
+
 
 # Register your models here.
+
+@admin.register(Store)
+class StoreAdmin(ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ('title', 'store_name')
+
+    def store_name(self, obj: Product):
+        return obj.store.name

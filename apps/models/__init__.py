0a9046cb507abeab54cfbfddc427bfdc4e075ab1
xolmomin <xolmomin@gmail.com>
2023-01-05 18:13:19 +0500

    add postgres db

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,2 +1,3 @@
 from apps.models.stores import Store
 from apps.models.base import Product, Stream, Comment
+# from apps.models.users import User
--- apps/models/users.py
+++ apps/models/users.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django_resized import ResizedImageField
-
-
-class User(AbstractUser):
-    phone = CharField(max_length=255, unique=True)
-    image = ResizedImageField(size=[200, 200], crop=['middle', 'center'], upload_to='users')
+# from django.contrib.auth.models import AbstractUser
+# from django.db.models import CharField
+# from django_resized import ResizedImageField
+#
+#
+# class User(AbstractUser):
+#     phone = CharField(max_length=255, unique=True)
+#     image = ResizedImageField(size=[200, 200], crop=['middle', 'center'], upload_to='users')

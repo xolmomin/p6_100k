210ec86a7afc286457e2007f5dd13e8e8eb7d36c
rashideveloper <bubbless7456@gmail.com>
2023-01-07 18:30:58 +0500

    views

--- apps/views/__init__.py
+++ apps/views/__init__.py
@@ -1,9 +1,4 @@
 from apps.views.auth import WithdrawView, ProfileView
-from apps.views.base import MainPageView, ProductDetailView, StreamPageView
-
-from apps.views.base import MainPageView
-from apps.views.auth import WithdrawView
 from apps.views.base import MainPageView, ProductDetailView, StreamPageListView
 from apps.views.market import MarketListView
-
 from apps.views.store_detail import StoreDetailView

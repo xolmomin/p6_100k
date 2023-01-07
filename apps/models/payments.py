6d5b37a742d175071041522d4131fd8a4623e9d6
jakhongir <ganiyevuzb@gmail.com>
2023-01-07 16:31:01 +0500

    override users model

--- apps/models/payments.py
+++ apps/models/payments.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from django.db.models import Model, DecimalField, ForeignKey, CharField, TextChoices
+from django.db.models import Model, DecimalField, ForeignKey, CharField, TextChoices, CASCADE, DateTimeField
 
 
 class PaymentHistory(Model):
@@ -9,8 +9,9 @@ class PaymentHistory(Model):
         ACCEPTED = 'accepted'
         CANCELED = 'canceled'
 
-    user = ForeignKey('apps.User')
+    user = ForeignKey('apps.User', CASCADE)
     amount = DecimalField(max_digits=12, decimal_places=2)
     card_number = CharField(max_length=16)
     status = CharField(choices=StatusChoices.choices, default=StatusChoices.PENDING)
-    transaction_id = CharField(max_length=255, default=uuid4())
+    transaction_id = CharField(max_length=255, default=uuid4)
+    created_at = DateTimeField(auto_now_add=True)

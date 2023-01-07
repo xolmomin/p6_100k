4b8cf8b9ca0f1a6ff03b2fc83672ae7491ebfea3
muhammad <abduvaliyevmuhammad41@gmail.com>
2023-01-07 17:03:36 +0500

    add Stream

--- apps/models/base.py
+++ apps/models/base.py
@@ -1,12 +1,12 @@
 from django.db.models import BooleanField, Model, DateTimeField, SlugField, ForeignKey, PROTECT, TextField, CharField, \
-    CASCADE, IntegerField
+    CASCADE, IntegerField, Manager
 from django_resized import ResizedImageField
 
 
 class Product(Model):
     title = CharField(max_length=255)
-    main_picture = ResizedImageField(size=[500, 300], upload_to='%m')
-    price = IntegerField(max_length=200)
+    main_picture = ResizedImageField(size=[500, 300], upload_to='product/%m')
+    price = IntegerField()
     created_at = DateTimeField(auto_now_add=True)
     slug = SlugField(max_length=255, unique=True)
     store = ForeignKey('apps.Store', CASCADE)
@@ -32,8 +32,8 @@ class Comment(Model):
 
 class Stream(Model):
     name = CharField(max_length=255)
-    donation = CharField(max_length=20)  # hayriya uchun mablag`
-    reduce = CharField(max_length=20)  # narxini kamaytirish uchun mablag`
+    donation = CharField(max_length=20, default=0)  # hayriya uchun mablag`
+    reduce = CharField(max_length=20, default=0)  # narxini kamaytirish uchun mablag`
     user = ForeignKey('auth.User', CASCADE)  # oqim yaratgan foydalanuchi
     product = ForeignKey('apps.Product', CASCADE)  # oqim uchun mahsulot
     is_area = BooleanField(default=False)  # hududsiz qabul qilish
--- apps/views/base.py
+++ apps/views/base.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import DetailView, FormView
+from django.views.generic import DetailView, FormView, ListView
 
 from apps.forms.base import CreateCommentForm
-from apps.models import Product, Comment
+from apps.models import Product, Comment, Stream
 
 from django.views.generic import TemplateView
 
@@ -23,3 +25,21 @@ class ProductDetailView(FormView, DetailView):
         context = super().get_context_data(object_list=object_list, **kwargs)
         context['comment'] = Comment.objects.all()
         return context
+
+
+def completed(request, *args, **kwargs):
+    stream = Stream.objects.get(id=id)
+    stream.is_area = not stream.is_area
+    stream.save()
+
+    return JsonResponse({'status': 200})
+
+# class StreamPageView(LoginRequiredMixin, ListView):
+class StreamPageView(ListView):
+    template_name = 'apps/admin/stream.html'
+    model = Stream
+    queryset = Stream.objects.all()
+    context_object_name = 'streams'
+    paginate_by = 9
+
+

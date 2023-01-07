f57ce044c5b47b1fe43adccb2e8823487ed7d658
adkahmjon <adkhamjonmakmudov@gmail.com>
2023-01-07 18:23:26 +0500

    update url

--- apps/urls.py
+++ apps/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
-from apps.views import WithdrawView, MarketListView, MainPageView, ProductDetailView, StreamPageView
-from apps.views import WithdrawView, MarketListView, MainPageView, ProductDetailView, ProfileView
-from apps.views.base import StreamPageView
-from apps.views import WithdrawView, MainPageView, ProductDetailView, MarketListView, StreamPageListView
+from apps.views import ProfileView
+from apps.views import WithdrawView, MarketListView, StreamPageListView
 from apps.views.base import MainPageView, ProductDetailView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page_view'),
     path('product/shop/<str:slug>', ProductDetailView.as_view(), name='product_detail'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('profile', ProfileView.as_view(), name='profile_page'),
     path('product/<str:slug>', ProductDetailView.as_view(), name='product_detail_view'),
     path('admin/stream', StreamPageListView.as_view(), name='stream_page_view'),
     path('admin/withdraw', WithdrawView.as_view(), name='withdraw'),

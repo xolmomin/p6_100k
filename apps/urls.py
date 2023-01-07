e77511896c831a41fe4637d61a492152bde36637
muhammad <abduvaliyevmuhammad41@gmail.com>
2023-01-07 18:25:50 +0500

    add

--- apps/urls.py
+++ apps/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('product/shop/<str:slug>', ProductDetailView.as_view(), name='product_detail'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('product/<str:slug>', ProductDetailView.as_view(), name='product_detail_view'),
-    path('admin/stream', StreamPageListView.as_view(), name='stream_page_view'),
+    path('admin/streams', StreamPageListView.as_view(), name='stream_page_view'),
     path('admin/withdraw', WithdrawView.as_view(), name='withdraw'),
     path('admin/market', MarketListView.as_view(), name='market'),
 ]

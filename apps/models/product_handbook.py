e7a0c877b45fac0517da67dccfb79ae2ae3dbd59
Ahrorbek <admin@mail.ru>
2023-01-07 16:42:37 +0500

    new functions

--- apps/models/product_handbook.py
+++ apps/models/product_handbook.py
@@ -1,15 +1,27 @@
 from django.db.models import Model, CharField, IntegerField, DateTimeField, SlugField, ForeignKey, CASCADE
 from django_resized import ResizedImageField
 
-
-class Product(Model):
+class Category(Model):
     title = CharField(max_length=255)
-    main_picture = ResizedImageField(size=[500, 300], upload_to='%m')
-    price = IntegerField()
-    created_at = DateTimeField(auto_now_add=True)
     slug = SlugField(max_length=255, unique=True)
-    store = ForeignKey('apps.Store', CASCADE)
 
-    class Meta:
-        verbose_name = "Product"
-        verbose_name_plural = "Products"
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            while Category.objects.filter(slug=self.slug).exists():
+                slug = Category.objects.filter(slug=self.slug).first().slug
+                if '-' in slug:
+                    try:
+                        if slug.split('-')[-1] in self.title:
+                            self.slug += '-1'
+                        else:
+                            self.slug = '-'.join(slug.split('-')[:-1]) + '-' + str(int(slug.split('-')[-1]) + 1)
+                    except:
+                        self.slug = slug + '-1'
+                else:
+                    self.slug += '-1'
+        super().save(*args, **kwargs)
+
+    @property
+    def product_count(self):
+        return self.product_set.count()
--- apps/models/products.py
+++ apps/models/products.py
@@ -1,4 +1,5 @@
-from django.db.models import Model, CharField, IntegerField, DateTimeField, SlugField, ForeignKey, CASCADE
+from django.db.models import Model, CharField, IntegerField, DateTimeField, SlugField, ForeignKey, CASCADE, BooleanField
+from django.utils.text import slugify
 from django_resized import ResizedImageField
 
 
@@ -8,8 +9,29 @@ class Product(Model):
     price = IntegerField()
     created_at = DateTimeField(auto_now_add=True)
     slug = SlugField(max_length=255, unique=True)
+    bonus = IntegerField()
+    free_delivery = BooleanField(default=False)
+    reserve = IntegerField()
     store = ForeignKey('apps.Store', CASCADE)
+    category = ForeignKey('apps.Category', CASCADE)
 
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            while Product.objects.filter(slug=self.slug).exists():
+                slug = Product.objects.filter(slug=self.slug).first().slug
+                if '-' in slug:
+                    try:
+                        if slug.split('-')[-1] in self.title:
+                            self.slug += '-1'
+                        else:
+                            self.slug = '-'.join(slug.split('-')[:-1]) + '-' + str(int(slug.split('-')[-1]) + 1)
+                    except:
+                        self.slug = slug + '-1'
+                else:
+                    self.slug += '-1'
+        super().save(*args, **kwargs)

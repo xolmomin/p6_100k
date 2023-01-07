8c6dedd47af1edbd72de011087f5dcff9f665de6
kkirchh <jasurkkirchh@gmail.com>
2023-01-07 16:07:17 +0500

    settings.py

--- root/settings.py
+++ root/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': 5432,
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 

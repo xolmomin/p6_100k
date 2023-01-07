34679e42203663103e559a143fb7875038612e81
jakhongir <ganiyevuzb@gmail.com>
2023-01-07 16:09:33 +0500

    uncommented User

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -3,4 +3,4 @@ from apps.models.stores import Store
 from apps.models.base import Product
 from apps.models.base import Product, Stream
 from apps.models.comments import Comment
-# from apps.models.users import User
+from apps.models.users import User

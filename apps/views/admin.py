e8a7875c06990c27cd98bf5bb370389b7b3557d9
muhammad <abduvaliyevmuhammad41@gmail.com>
2023-01-24 14:29:07 +0500

    add

--- apps/views/admin.py
+++ apps/views/admin.py
@@ -10,7 +10,8 @@ from apps.utils import statistic_query
 class MarketListView(LoginRequiredMixin, ListView, CreateView, FormView):
     template_name = 'apps/admin/market_page.html'
     paginate_by = 15
-    model = Stream
+    queryset = Product.objects.order_by('id')
+    context_object_name = 'products'
     object = Stream
     form_class = CreateStreamForm
     success_url = reverse_lazy('market')
@@ -23,7 +24,6 @@ class MarketListView(LoginRequiredMixin, ListView, CreateView, FormView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
         context['categories'] = Category.objects.all()
-        context['products'] = Product.objects.order_by('id')
 
         search_input = self.request.GET.get('search') or ''
         context['search_input'] = search_input
